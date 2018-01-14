176fa573d8d783f9eb997c46b1d1ab5ac90376ee
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-14 15:17:06 -0800

    test: do not reset env for windows compat

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, output} from '@dxcli/dev-test'
 
 import {run} from '../src'
 
-describe.env().stdout('run', () => {
+describe.stdout('run', () => {
   it('example', async () => {
     await run(['node', 'js', 'example'])
     expect(output.stdout).to.contain('[echo] example workflow')
